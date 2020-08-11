d0b637e3c8a65dc4a72332fe338f2ddfdede3806
CptConi <nicolas.renard.33@gmail.com>
2020-08-11 11:03:14 +0200

    Sauces controller

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,9 @@
+const Sauce = require("../models/things");
+const fs = require("fs");
+
+exports.getAllSauces = (req, res, next) => {};
+exports.getOneSauce = (req, res, next) => {};
+exports.createSauce = (req, res, next) => {};
+exports.likeSauce = (req, res, next) => {};
+exports.modifySauce = (req, res, next) => {};
+exports.deleteSauce = (req, res, next) => {};
