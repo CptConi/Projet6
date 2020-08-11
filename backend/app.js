092016341e9387ac8915de7a92a692a204a90491
CptConi <nicolas.renard.33@gmail.com>
2020-08-11 10:45:19 +0200

    Backend files

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,14 @@ const { json } = require("body-parser");
 const path = require("path");
 
 const app = express();
-/*
-*       INSERT REQUIRED ROUTES HERE
-*/
+
+//Routes
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces')
 
 //Connexion à la DB MongoDB:
 //Login: So_Pekocko_Admin_Mongo
 //Password >> piGWDRfWwp3VONBq
-
 mongoose
   .connect(
     "mongodb+srv://So_Pekocko_Admin_Mongo:piGWDRfWwp3VONBq@sopekocko.nwyga.mongodb.net/<dbname>?retryWrites=true&w=majority",
@@ -21,30 +21,25 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-// On indique ici les middleware gérant la réponse de notre app back-end
 // Set le HEADER pour qu'il accepte les requêtes Cross Origin (Et empêcher les bugs de CORS)
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 
 app.use(bodyParser.json());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-//
-//       INSERT ROUTES HERE
-//       
-// app.use("/api/auth", userRoutes);
-// app.use("/api/sauces");
-
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", saucesRoutes);
 
 module.exports = app;
--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,8 +4,19 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
-const stuffCtrl = require("../controllers/sauces");
+const saucesCtrl = require("../controllers/sauces");
 
 // Les routes fonction des requêtes à l'API:
+
+router.get("/", auth, saucesCtrl.getAllSauces);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
+
+router.post('/', auth, multer, saucesCtrl.CreateSauce);
+router.post("/:id/like", auth, saucesCtrl.likeSauce);
+
+router.put("/:id", auth, multer, saucesCtrl.modifySauce);
+
+router.delete("/:id", auth, saucesCtrl.deleteSauce);
+
 
 module.exports = router;
